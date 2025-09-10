d473708226f45bc5c0e744307ff4366a7b202c38
Starway <148691101+CodeLikeBA56@users.noreply.github.com>
2025-09-10 12:17:58 +0500

    Delete Hospital Management System(Emergency) directory

--- Hospital Management System(Emergency)/Project/DatabaseConnection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $_Hostname = "localhost";
-    $_Username = "root";
-    $_Password = "";
-    $_Database = "hms";
-
-    $conn = mysqli_connect($_Hostname, $_Username, $_Password, $_Database) or die("Database Connection Failed.");
-?>
